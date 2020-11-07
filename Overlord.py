10f5b237812ebee76c9267238c25d249f4f16c09
razberries <54728263+razberries@users.noreply.github.com>
2020-11-06 23:04:14 -0500

    add color

--- Overlord.py
+++ Overlord.py
@@ -20,12 +20,21 @@ OverlordMenu()
 select = int(input("Which option?: "))
 
 
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 ina = None
 
 
 def cmd():
-	argz = input(ina + "@overlord $ ")
+	argz = input(bcolors.OKCYAN + ina + "@overlord $ " + bcolors.ENDC)
 	os.system(argz)
 while select != 1:
 	if select == 2:
